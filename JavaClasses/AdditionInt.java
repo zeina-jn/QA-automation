526fbef30280713d009f9a877d8d3dffadebb4db
zeina-jn <zeinab.jnaydi.12@gmail.com>
2022-10-27 21:57:33 +0300

    Sum of 2 numbers

---
+++ JavaClasses/AdditionInt.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class AdditionInt {
+    public static void main (String [] args) {
+Scanner sc = new Scanner (System.in);
+
+System.out.println("the first number is ");
+   int a = sc.nextInt();
+   System.out.println("the 2nd number is ");
+
+   int b = sc.nextInt();
+
+      System.out.println("the sum of "+a+" and " +b+  " = "+ (a+b));
+
+        // System.out.println("the sum of  a + b = "+ (a+b));
+//double
+//double b = sc.nextDouble();
+
+    }
+}
