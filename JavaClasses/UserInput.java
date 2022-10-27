a9d2bd7ae2d55099bab983aa35b8c063e4eeb26e
Apple <Apple@apples-MacBook-Pro.local>
2022-10-27 20:21:27 +0300

    userInput example

---
+++ JavaClasses/UserInput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class UserInput {
+    public static void main (String [] args) {
+Scanner sc = new Scanner (System.in);
+    System.out.println("what's your name");
+    String Username = sc.next();
+    System.out.println("my name is  : "+ Username);
+    }
+}
+
