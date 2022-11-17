13f19636c49f54e51ca12479387164e2ff2ac9c5
zeina-jn <zeinab.jnaydi.12@gmail.com>
2022-11-17 23:59:47 +0200

    count specific character

---
+++ src/package1/Count_char.java
@@ -0,0 +1,22 @@
+package package1;
+
+public class Count_char {
+
+	public static void main(String[] args) {
+		String s = "java developer";
+		int c = 0 ;
+		
+		for (int i =0 ; i <s.length();i++) {
+			char a = s.charAt(i);
+			
+			if (a == 'e') {
+				c++;
+			}
+		}
+		
+		System.out.println("count of 'e' : "+c);
+
+
+	}
+
+}
