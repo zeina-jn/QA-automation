5546a519aa42d8f18cf6d07676957a98c8019cd6
zeina-jn <zeinab.jnaydi.12@gmail.com>
2022-11-17 23:14:02 +0200

    string method

---
+++ src/package1/String_method.java
@@ -0,0 +1,17 @@
+package package1;
+
+public class String_method {
+
+	public static void main(String[] args) {
+		
+		String s = "java developer";
+		
+		 System.out.println("index of 'e' is : "+s.indexOf('e'));
+
+		 System.out.println("last index of 'e' is : "+s.lastIndexOf('e'));
+
+		 System.out.println("index of 'b' is : "+s.indexOf('b'));//return -1
+
+	}
+
+}
