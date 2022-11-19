94cd8f14578d9b7db2231b85e3a3d921ae545227
zeina-jn <zeinab.jnaydi.12@gmail.com>
2022-11-19 19:22:34 +0200

    upper & lower cases

---
+++ src/package1/UpperandLower.java
@@ -0,0 +1,30 @@
+package package1;
+
+public class UpperandLower {
+
+	public static void main(String[] args) {
+		
+		String s = "Java Developer";
+
+		System.out.println("reverse is :"+changecase(s));
+
+	}
+	
+	public static String changecase(String s) {
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i =0 ; i<s.length();i++ ) {
+			char c = s.charAt(i);
+			if (c>='A' && c<='Z') {
+				sb.append(Character.toLowerCase(c));
+			}else if (c>='a' && c<='z') {
+				sb.append(Character.toUpperCase(c));
+
+			}else {
+				sb.append(" ");
+			}
+		}
+		return sb.toString();
+	}
+
+}
