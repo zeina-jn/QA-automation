4aa97def567260ab69dfc5ac85ef25d49c75a0b6
zeina-jn <zeinab.jnaydi.12@gmail.com>
2022-11-20 17:21:58 +0200

    Assignment 2

--- src/com/arrays/Reverse_Sentence.java
+++ src/com/arrays/Reverse_Sentence.java
@@ -4,7 +4,7 @@ public class Reverse_Sentence {
 
 	public static void main(String[] args) {
 
-		String s = "Java Is Very Easy Language";
+		String s = "JavReverse_Sentence a Is Very Easy Language";
 		
 		String s1 [] = s.split(" ");
 		String s2 = "";
