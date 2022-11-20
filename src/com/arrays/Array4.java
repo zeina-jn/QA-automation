888903a62d46735bdcc0164c7612a5b3cad4c460
zeina-jn <zeinab.jnaydi.12@gmail.com>
2022-11-20 19:12:20 +0200

    sort array

--- src/com/arrays/Array4.java
+++ src/com/arrays/Array4.java
@@ -1,5 +1,16 @@
 package com.arrays;
 
-public class Array4 {
+import java.util.Arrays;
 
+public class Array4 {
+	
+	public static void main(String[] args) {
+	    int [] array = {2,6,7,3,8,22,44,12,55,33,};
+       // System.out.println(Arrays.sort(array)); 
+	    Arrays.sort(array);// sort in ascending order
+	    
+	    for (int a:array) {
+	    	System.out.println(a);
+	    }
+	}
 }
