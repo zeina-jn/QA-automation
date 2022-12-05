f479eab6839ad3e23a229271ba4abb773d49ce9a
zeina-jn <zeinab.jnaydi.12@gmail.com>
2022-12-05 21:56:24 +0200

    selenium

---
+++ src/com/selenium/ActiveTime_login.java
@@ -0,0 +1,13 @@
+package com.selenium;
+
+
+
+public class ActiveTime_login {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver","/Users/Apple 1/Documents/chromedriver_mac64/chromedriver");
+       
+     // driver.quit();
+	}
+}
